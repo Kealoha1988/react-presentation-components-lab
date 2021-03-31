cc1192cf227f20889384d04f3f2baa9818ce8257
Kealoha1988 <kealohaabe1988@gmail.com>
2021-03-31 13:17:30 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,24 @@
 // Code SimplerComponent Here
+import React, { Component } from 'react'
+
+export class SimplerComponent extends Component {
+constructor(props) {
+  super(props)
+
+  this.state = {
+    mood: this.props.mood
+  }
+}
+
+
+  render() {
+    return (
+      <div>
+        <h2>Hello{this.state.mood}</h2>
+        
+      </div>
+    )
+  }
+}
+
+export default SimplerComponent
